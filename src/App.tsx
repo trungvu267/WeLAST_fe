5125b4246d0feb2bedcc14d7a9e4fc7c55c2812b
Trung <vutrung26072001@gmail.com>
2024-08-30 15:45:00 +0700

    feat: use repo dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,45 @@
-	import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 // services
-import fetchRepos from './services/fetch-repos'
+import fetchRepos from "./services/fetch-repos";
 
 // components
-import LanguageCategoryTab from './components/language-category-tab';
-import {  groupByLang } from './helpers';
+import LanguageCategoryTab from "./components/language-category-tab";
+import RepoDialog from "./components/repo-dialog";
+
+// helpers
+import { groupByLang } from "./helpers";
 function App() {
+	const [loading, setLoading] = useState(false);
+	const [langCategories, setLangCategories] = useState<Category[]>([]);
 
-  const [loading, setLoading] = useState(false);
-  const [langCategories,setLangCategories]= useState<Category[]>([])
+	useEffect(() => {
+		const getRepos = async () => {
+			try {
+				setLoading(true);
+				const res = await fetchRepos();
+				if (res?.length) {
+					setLangCategories(groupByLang(res));
+				}
+			} catch (error) {
+				console.error("Error fetching repos:", error);
+			} finally {
+				setLoading(false);
+			}
+		};
 
-  useEffect(() => {
-    const getRepos = async () => {
-      try {
-        setLoading(true); 
-        const res = await fetchRepos();
-		if(res?.length){
-			setLangCategories(groupByLang(res))
-		}
-      } catch (error) {
-        console.error("Error fetching repos:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+		getRepos();
+	}, []);
 
-    getRepos();
-  }, []);
-  
-  if(loading && !langCategories) return <p>Loading...</p>
+	if (loading && !langCategories) return <p>Loading...</p>;
 
-  return <LanguageCategoryTab categories={langCategories}/>
+	return (
+		<>
+			<LanguageCategoryTab categories={langCategories} />
+			<RepoDialog />
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/components/repo-item.tsx
+++ src/components/repo-item.tsx
@@ -1,17 +1,36 @@
-const RepoItem = ({repo}:{repo:Repo}) => {
-  return (
-	<li  className="relative rounded-md p-3 text-sm/6 transition bg-white/5 hover:bg-white/10 col-span-1">
-                      <a href="#" className="font-semibold text-lime-300">
-                        <span className="absolute inset-0" />
-                        {repo.name}
-                      </a>
-                      <ul className="gap-2 text-white/50 list-none">
-                        <li><span className='font-semibold text-white'>Desc: </span> {repo.description || 'N/A'}</li>
-                        <li><span className='font-semibold text-white'>Language: </span> {repo.language || 'N/A'}</li>
-                        <li><span className='font-semibold text-white'>Forks count: </span>{repo.forks_count || 'N/A'}</li>
-                      </ul>
-                    </li>
-  )
-}
+import { useSelectRepo } from "../stores";
 
-export default RepoItem
+const RepoItem = ({ repo }: { repo: Repo }) => {
+	const { setSelectRepo } = useSelectRepo();
+	return (
+		<li
+			className="relative rounded-md p-3 text-sm/6 transition bg-white/5 hover:bg-white/10 col-span-1"
+			onClick={() => {
+				setSelectRepo(repo);
+			}}
+		>
+			<a href="#" className="font-semibold text-lime-300">
+				<span className="absolute inset-0" />
+				{repo.name}
+			</a>
+			<ul className="gap-2 text-white/50 list-none">
+				<li>
+					<span className="font-semibold text-white">Desc: </span>{" "}
+					{repo.description || "N/A"}
+				</li>
+				<li>
+					<span className="font-semibold text-white">Language: </span>{" "}
+					{repo.language || "N/A"}
+				</li>
+				<li>
+					<span className="font-semibold text-white">
+						Forks count:{" "}
+					</span>
+					{repo.forks_count || "N/A"}
+				</li>
+			</ul>
+		</li>
+	);
+};
+
+export default RepoItem;
